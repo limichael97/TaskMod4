0a9767da708e72ceb8338d5823cd91c8ec676e3c
Michael <michael@thinkgrow.com>
2022-11-11 20:52:32 -0800

    added form

--- assets/js/script 2.js
+++ assets/js/script 2.js
@@ -1,15 +1,11 @@
+var buttonEl = document.querySelector("#save-task")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var formEl = document.querySelector("#task-form");
 
-
-var createTaskHandler = function(event) {
-
-    event.preventDefault()
-
+var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-formEl.addEventListener("submit", createTaskHandler)
+buttonEl.addEventListener("click", createTaskHandler)
